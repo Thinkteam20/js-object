2be1b987d56b6a2e36c4a9037e0e606e00d05d72
Sangpil Jung <tankteam20@gmail.com>
2021-01-30 00:08:55 +1000

    usage of this

---
+++ this.js
@@ -0,0 +1,12 @@
+const Jung = {
+    name: 'Jung',
+    first: 10,
+    second:20,
+    sumScore(){
+        return this.first + this.second
+    }
+};
+
+//console.log(Jung.sumScore(Jung.first,Jung.second));
+
+console.log(Jung.sumScore());
